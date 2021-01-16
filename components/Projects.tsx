a1e8e597a264df7417b2a00e2071dec728f18a6d
Senhung Wong <alexwongsenhung@gmail.com>
2021-01-16 23:12:23 +0800

    Adjust navbar

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -15,7 +15,9 @@ export const Projects: React.FC = () => {
           </h2>
           <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-12">
             {PROJECTS.map(({ src, href }) => (
-              <Project src={src} href={href} />
+              <React.Fragment key={src}>
+                <Project src={src} href={href} />
+              </React.Fragment>
             ))}
           </div>
         </div>
